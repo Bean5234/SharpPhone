974eecc1ab21c17a2e498b60bb420e2de539980f
deanv <deanv@LAPTOP-K90U3OKG>
2026-02-12 09:14:00 +0100

    Add project files.

---
+++ SharpPhone/Smartphone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpPhone
+{
+    internal class SmartPhone
+    {
+        private int id;
+        private String brand;
+        private String model;
+        private double storageSizeMb;
+        private double price;
+
+
+        public int Id;
+        public String Brand;
+        public String Model;
+        public double StorageSizeMb;
+        public double Price;
+
+
+    }
+}
